66ae8d5e51328318b07e7877f56522d362ab60a2
Claudemir Macedo <yanalogic@gmail.com>
2025-07-09 09:55:07 -0300

    feat: add react query

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { queryClient } from './src/infra/repositories/query/query-client';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </QueryClientProvider>
   );
 }
 
