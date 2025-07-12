3e49f680e55bc5ec9eb80ec89aaeccb20973cc8e
Claudemir Macedo <yanalogic@gmail.com>
2025-07-12 10:53:20 -0300

    fix: query import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { queryClient } from './src/infra/repositories/query/query-client';
+import { queryClient } from './src/infra/query/query-client';
 
 export default function App() {
   return (
