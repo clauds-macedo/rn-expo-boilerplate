87bb25b3b0b1f43033f21cde66c21ce5d616baaa
Claudemir Macedo <yanalogic@gmail.com>
2025-07-12 10:52:44 -0300

    chore: move query folder

--- src/infra/repositories/persistence/expo-secure-store-repository.ts
+++ src/infra/repositories/persistence/expo-secure-store-repository.ts
@@ -2,15 +2,15 @@ import * as SecureStore from "expo-secure-store";
 import type { IPersistenceRepository } from "./persistence-repository-contract";
 
 export const expoSecureStoreRepository: IPersistenceRepository = {
-	get(key: string): string | null {
-		return SecureStore.getItem(key);
-	},
+  get(key: string): string | null {
+    return SecureStore.getItem(key);
+  },
 
-	set(key: string, value: string): void {
-		SecureStore.setItem(key, value);
-	},
+  set(key: string, value: string): void {
+    SecureStore.setItem(key, value);
+  },
 
-	remove(key: string): Promise<void> {
-		return SecureStore.deleteItemAsync(key);
-	},
+  remove(key: string): Promise<void> {
+    return SecureStore.deleteItemAsync(key);
+  },
 };
